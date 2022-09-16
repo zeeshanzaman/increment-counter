c3c2c8bdaaefd4dcbdb4eed013edc780430e1743
zeeshanzaman <88842812+zeeshanzaman@users.noreply.github.com>
2022-09-16 17:07:43 +0500

    update counter

--- custom.js
+++ custom.js
@@ -1,5 +1,13 @@
 const counters = document.querySelectorAll('.counter')
 
 counters.forEach(counter => {
-    counter.innerText = '0'
-})
+        counter.innerText = '0'
+
+        const updateCounter = () => {
+            const target = counter.getAttribute('data-target')
+            console.log(typeof target, target)
+        }
+
+    })
+    // 62 lenght 
+    // 23 bazzuu
