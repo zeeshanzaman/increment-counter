d30e891a1f0b32ddf459fb20e76049e2e4cbea15
zeeshanzaman <88842812+zeeshanzaman@users.noreply.github.com>
2022-09-16 18:02:45 +0500

    make if else condition and set time out

--- custom.js
+++ custom.js
@@ -4,9 +4,20 @@ counters.forEach(counter => {
         counter.innerText = '0'
 
         const updateCounter = () => {
-            const target = counter.getAttribute('data-target')
-            console.log(typeof target, target)
+            const target = +counter.getAttribute('data-target')
+            const c = +counter.innerText
+
+            const increment = target / 1000
+
+            if (c < target) {
+                counter.innerText = `${Math.ceil(c + increment)}`
+                setTimeout(updateCounter, 1)
+            } else {
+                counter.innerText = target
+            }
         }
+
+        updateCounter()
 
     })
     // 62 lenght 
